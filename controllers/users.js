6d7e7cccd6d719168a2d0439e8597250b8538854
Em774 <e.dussaix74@gmail.com>
2017-08-14 15:24:08 +0100

    changes some stuff

--- controllers/users.js
+++ controllers/users.js
@@ -18,6 +18,21 @@ function usersDelete(req, res) {
 
 function usersShow(req, res) {
 
+  //make sure to imbed the items
+//   function usersShow(req, res) {
+//   User
+//     .findById(req.params.id)
+//     .populate('trip')
+//     .exec()
+//     .then((user) => {
+//       Trip
+//         .find({createdBy: user._id})
+//         .exec()
+//         .then(trips => {
+//           res.render('users/show', {user, trips});
+//         });
+//     });
+// }
 }
 
 module.exports = {

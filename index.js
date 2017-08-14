48fcda87f55ba4e288c4e0793994eff13d676c7d
Em774 <e.dussaix74@gmail.com>
2017-08-14 10:21:44 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
-// const port    = process.env.PORT || 4000;
 const app     = express();
 const dest    = `${__dirname}/public`;
-
 const mongoose   = require('mongoose');
 const bluebird   = require('bluebird');
 const morgan     = require('morgan');
@@ -16,14 +14,28 @@ const routes     = require('./config/routes');
 mongoose.connect(config.db);
 mongoose.Promise = bluebird;
 
-
+if (app.get('env') !== 'production') app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded: true}));
+app.use(morgan('dev'));
 app.use(express.static(dest));
 
+app.use('/api', expressJWT({secret: config.secret})
+.unless({
+  path: [
+    {url: '/api/register', methods: ['POST']},
+    {url: '/api/login', methods: ['POST']}
+  ]
+}));
 
+app.use(jwtErrorHandler);
 
+function jwtErrorHandler(err, req, res, next){
+  if(err.name !== 'UnauthorizedError') return next();
+  return res.status(401).json({ message: 'Unauthorized request.'});
+}
 
-
-
+app.use('/api', routes);
 app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
 
-app.listen(port, () => console.log(`Express has started on port: ${port}`));
+app.listen(config.port, () => console.log(`Express has started on port: ${config.port}`));

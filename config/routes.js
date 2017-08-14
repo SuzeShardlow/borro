9820338df1b0285d76c3939a9728ddd7c73cc92e
Em774 <e.dussaix74@gmail.com>
2017-08-14 15:04:23 +0100

    added routes for authentication

--- config/routes.js
+++ config/routes.js
@@ -3,12 +3,12 @@ const router          = express.Router();
 const items           = require('../controllers/items');
 const users           = require('../controllers/users');
 
-// const authentications = require('../controllers/authentications');
+const authentications = require('../controllers/authentications');
 
-// router.route('/register')
-//   .post(authentications.register);
-// router.route('/login')
-//   .post(authentications.login);
+router.route('/register')
+  .post(authentications.register);
+router.route('/login')
+  .post(authentications.login);
 
 router.route('/items')
   .get(items.index);

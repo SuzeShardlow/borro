dc294d75834ebc33f79aea7810c8fa0b3bc49d93
SuzeShardlow <suze.shardlow1@gmail.com>
2017-08-15 11:30:15 +0100

    created delete modal

--- src/js/controllers/items/items-delete.controller.js
+++ src/js/controllers/items/items-delete.controller.js
@@ -6,10 +6,6 @@ ItemsDeleteCtrl.$inject = ['$uibModalInstance', 'item', '$state', 'Item'];
 function ItemsDeleteCtrl($uibModalInstance, item, $state, Item) {
   const vm = this;
 
-  // bird -> is the value being passed from the BirdsShowCtrl containing the object of the bird being shown on the state.
-
-  // Bird -> the factory allowing requests to be made to the API for the bird resource.
-
   vm.item            = item;
   vm.closeModal      = closeModal;
   vm.itemsDelete     = itemsDelete;

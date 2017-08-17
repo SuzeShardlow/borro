053ef50c913c05dfe72a36fb166a8212bec4f034
SuzeShardlow <suze.shardlow1@gmail.com>
2017-08-17 10:02:04 +0100

    bugs

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ module.exports = {
   port: process.env.PORT || 4000,
   db: {
     production: process.env.MONGODB_URI,
-    development:'mongodb://localhost/toolio',
-  secret: process.env.SECRET || 'jh4893754ehrfhr9834rhfrwhf4389rhfr',
-  test: 'mogodb://localhost/items-test'
+    development: 'mongodb://localhost:27017/toolio',
+    secret: process.env.SECRET || 'jh4893754ehrfhr9834rhfrwhf4389rhfr',
+    test: 'mongodb://localhost/items-test'
+  }
 };
